e1a2e0d4ac307cf5a01b91fb007476c15e84cb94
virtues1987 <virtues@ukr.net>
2016-07-11 11:52:44 +0300

    Update UserSettingsPage.java

--- src/test/java/functions/pages/UserSettingsPage.java
+++ src/test/java/functions/pages/UserSettingsPage.java
@@ -1,10 +1,36 @@
 package functions.pages;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import pages.CreatedBoardPage;
+import pages.MainPage;
+import pages.UserInfoPage;
+
 /*******************************************************************************
- * (C) Copyright
- * Logivations GmbH, Munich 2010-2016
+ * (C) Copyright Logivations GmbH, Munich 2010-2016
  ******************************************************************************/
 
-public class UserSettingsPage extends CreateNewBoard{
-
+public class UserSettingsPage extends CreateNewBoard {
+	MainPage mainPage;
+	UserInfoPage userInfoPage;
+	
+	 @BeforeTest
+	    @Parameters({"path", "login", "password"})
+	    public void createNewBoardPageInit(String path, String login, String password) {
+	        driver.get(path);
+	        firstForm(login, password);
+	        secondForm(login, password);
+	        mainPage = PageFactory.initElements(driver, MainPage.class);
+	        userInfoPage = PageFactory.initElements(driver, UserInfoPage.class);
+	 }
+	 
+	 @Test
+	 public void testPage(){
+		 Assert.assertEquals(userInfoPage.getTitle(), "Pinterest • The world’s catalog of ideas");
+		 Assert.assertTrue(userInfoPage.isEditButtonDisplayed());
+	 }
 }
